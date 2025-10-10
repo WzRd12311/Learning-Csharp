bfaa87e554f329321735414e1f3c41587484b0f7
Denyslav Volin-Danilov <netranner22845@gmail.com>
2025-10-10 14:42:40 +0300

    add form 2

--- College/PR4/PR4/FormEx2.Designer.cs
+++ College/PR4/PR4/FormEx2.Designer.cs
@@ -28,30 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label3 = new System.Windows.Forms.Label();
+            this.Result = new System.Windows.Forms.Label();
             this.btnCheck = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.Element = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.CurArr = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // label3
+            // Result
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(180, 167);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(118, 13);
-            this.label3.TabIndex = 9;
-            this.label3.Text = "Результат перевірки: ";
+            this.Result.AutoSize = true;
+            this.Result.Location = new System.Drawing.Point(180, 201);
+            this.Result.Name = "Result";
+            this.Result.Size = new System.Drawing.Size(118, 13);
+            this.Result.TabIndex = 9;
+            this.Result.Text = "Результат перевірки: ";
             // 
             // btnCheck
             // 
-            this.btnCheck.Location = new System.Drawing.Point(183, 116);
+            this.btnCheck.Location = new System.Drawing.Point(183, 150);
             this.btnCheck.Name = "btnCheck";
             this.btnCheck.Size = new System.Drawing.Size(171, 23);
             this.btnCheck.TabIndex = 8;
             this.btnCheck.Text = "Перевірити симетричність";
             this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
             // 
             // label2
             // 
@@ -68,6 +70,7 @@
             this.Element.Name = "Element";
             this.Element.Size = new System.Drawing.Size(171, 20);
             this.Element.TabIndex = 6;
+            this.Element.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Element_KeyUp);
             // 
             // label1
             // 
@@ -79,12 +82,22 @@
     "им вважається масив, який однаково читається зліва направо і праворуч\r\nналіво).\r" +
     "\n";
             // 
+            // CurArr
+            // 
+            this.CurArr.AutoSize = true;
+            this.CurArr.Location = new System.Drawing.Point(180, 115);
+            this.CurArr.Name = "CurArr";
+            this.CurArr.Size = new System.Drawing.Size(110, 13);
+            this.CurArr.TabIndex = 10;
+            this.CurArr.Text = "Теперешній масив:  ";
+            // 
             // FormEx2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(484, 261);
-            this.Controls.Add(this.label3);
+            this.Controls.Add(this.CurArr);
+            this.Controls.Add(this.Result);
             this.Controls.Add(this.btnCheck);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.Element);
@@ -98,10 +111,11 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label Result;
         private System.Windows.Forms.Button btnCheck;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox Element;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label CurArr;
     }
 }
--- College/PR4/PR4/FormEx2.cs
+++ College/PR4/PR4/FormEx2.cs
@@ -12,9 +12,51 @@ namespace PR4
 {
     public partial class FormEx2 : Form
     {
+
+        private object[] array = new object[7];
+        private int count = 0;
+
         public FormEx2()
         {
             InitializeComponent();
+        }
+
+        private void Element_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter) {
+
+                if (Element.Text.Length > 5) {
+                    MessageBox.Show("Елемент масиву завеликий.\n Максимальный розмір елементу = 5", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    Element.Clear();
+                }
+
+                if (count < 7) {
+                    CurArr.Text += Element.Text + ' ';
+                    array[count++] = Element.Text;
+                    Element.Clear();
+                }
+
+                else
+                {
+                    MessageBox.Show("Масив повний", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    Element.Clear();
+                }
+            }
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            if (count < 7)
+            {
+                MessageBox.Show("Масив не заповнений", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            bool isSymmetric = array.SequenceEqual(array.Reverse());
+            Result.Text = $"Результат перевірки: {isSymmetric}";
+
+
+
         }
     }
 }
